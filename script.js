6e5d98026b148ecd8f9b18bea218019d0e03196d
Danil Sadovoy <53143990+Sadovoy-Danil@users.noreply.github.com>
2019-07-27 18:16:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Hello World!'); // Read only
+
+alert('Hello World!'); // OK
+
+let answer = confirm('Are you here?'); // Cancel/OK
+
+console.log(answer);
+
+let question = prompt('Есть ли вам 18?'); // Something
+
+console.log(question);
+
+console.log(typeof(question));
